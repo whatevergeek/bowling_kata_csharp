7bef97520cc845c29d9809513704f8341f66aff6
Paul Lorett Amazona <whatevergeek@gmail.com>
2019-06-08 16:49:44 +0800

    skeleton

---
+++ BowlingGameTest/BowlingGameTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BowlingKata
+{
+    public class BowlingGameTest
+    {
+        [Fact]
+        public void TestRollsAllEmpty()
+        {
+            
+        }
+    }
+}
